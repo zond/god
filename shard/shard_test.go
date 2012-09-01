68582cfffa450c6f251952eb9eef41bc6a3a978c
Martin Bruse <martin@oort.se>
2012-09-01 16:34:05 +0200

    better tests

--- shard/shard_test.go
+++ shard/shard_test.go
@@ -3,24 +3,37 @@ package shard
 
 import (
 	"testing"
+	"reflect"
 )
 
-func TestShardLoading(t *testing.T) {
+func testPerform(t *testing.T, s *Shard, o Operation, expected Response) {
+	r := &Response{}
+	s.Perform(o, r)
+	if r.Result != expected.Result {
+		t.Errorf("s.Perform(%v, ...) expected %v but got %v", o, expected, r)
+	}
+	if !reflect.DeepEqual(r.Parts, expected.Parts) {
+		t.Errorf("s.Perform(%v, ...) expected %v but got %v", o, expected, r)
+	}
+}
+
+func TestShardBasicOps(t *testing.T) {
 	s, err := NewEmptyShard("test1")
 	if err != nil {
 		t.Errorf("while trying to create empty shard: %v", err)
 	}
-	r := &Response{}
-	s.Perform(Operation{PUT, []string{"k", "v"}}, r)
+	testPerform(t, s, Operation{GET, []string{"k"}}, Response{OK | MISSING, nil})
+	testPerform(t, s, Operation{PUT, []string{"k", "v"}}, Response{OK, nil})
+	testPerform(t, s, Operation{GET, []string{"k"}}, Response{OK | EXISTS, []string{"v"}})
 	s, err = NewShard("test1")
 	if err != nil {
 		t.Errorf("while trying to load shard: %v", err)
 	}
-	s.Perform(Operation{GET, []string{"k"}}, r)
-	if r.Result & OK != OK {
-		t.Errorf("should be OK/k: %v", r)
-	}
-	if r.Parts[0] != "v" {
-		t.Errorf("should be OK/k: %v", r)
-	}
+	testPerform(t, s, Operation{GET, []string{"k"}}, Response{OK | EXISTS, []string{"v"}})
+	testPerform(t, s, Operation{CLEAR, []string{}}, Response{OK, nil})
+	testPerform(t, s, Operation{GET, []string{"k"}}, Response{OK | MISSING, nil})
+	testPerform(t, s, Operation{PUT, []string{"k", "v"}}, Response{OK, nil})
+	testPerform(t, s, Operation{GET, []string{"k"}}, Response{OK | EXISTS, []string{"v"}})
+	testPerform(t, s, Operation{DELETE, []string{"k"}}, Response{OK, nil})
+	testPerform(t, s, Operation{GET, []string{"k"}}, Response{OK | MISSING, nil})
 }
