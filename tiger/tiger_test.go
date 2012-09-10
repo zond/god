016152051298e01bab350cb8d82096d60505fd0d
Martin Bruse <martin@oort.se>
2012-09-10 10:23:20 +0200

    ...

--- tiger/tiger_test.go
+++ tiger/tiger_test.go
@@ -10,8 +10,13 @@ import (
 
 func TestTiger(t *testing.T) {
 	tiger := &Tiger{}
-	if string(tiger.Sum([]byte("martin"))) != string([]byte{158,5,123,89,250,107,181,252,220,8,139,139,226,90,4,210,121,173,234,27,112,213,124,50}) {
-		t.Error("wrong checksum")
+	for i := 0; i < 1000; i++ {
+		s := fmt.Sprint(rand.Int63())
+		c1 := tiger.Sum([]byte(s))
+		c2 := tiger.Sum([]byte(s))
+		if string(c1) != string(c2) {
+			t.Errorf("%v should == %v", c1, c2)
+		}
 	}
 }
 
