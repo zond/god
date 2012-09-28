3fdb4be184e6a9f6ea83877fe0e32c97353fdfcb
Martin Bruse <martin@oort.se>
2012-09-28 23:59:52 +0200

    more work on the networking

--- server/server.go
+++ server/server.go
@@ -6,15 +6,9 @@ import (
 )
 
 func main() {
-	if s, err := shard.NewShard("239.255.4.5:7373"); err != nil {
-		panic(err)
-	} else {
-		s.Start()
-		if s2, err := shard.NewShard("239.255.4.5:7373"); err != nil {
-			panic(err)
-		} else {
-			s2.Start()
-			time.Sleep(time.Second * 10)
-		}
-	}
+	s := shard.NewShard("239.255.4.5:7373")
+	s2 := shard.NewShard("239.255.4.5:7373")
+	s.Start()
+	s2.Start()
+	time.Sleep(time.Second * 10)
 }
--- shard/shard.go
+++ shard/shard.go
@@ -6,14 +6,80 @@ import (
 	"net"
 )
 
-type Shard struct {
-	clusterAddress    *net.UDPAddr
-	clusterConnection *net.UDPConn
+const (
+	join = iota
+)
+
+type messageType byte
+
+func (self messageType) String() string {
+	switch self {
+	case join:
+		return "join"
+	}
+	return fmt.Sprintf("Unknown messageType: %#v", self)
 }
 
-func NewShard(address string) (result *Shard, err error) {
-	result = &Shard{}
-	result.clusterAddress, err = net.ResolveUDPAddr("udp", address)
+type udpMessage struct {
+	message messageType
+	source  *net.TCPAddr
+}
+
+func (self udpMessage) String() string {
+	return fmt.Sprintf("%v %v", self.message, self.source)
+}
+func (self udpMessage) encode() (result []byte) {
+	result = make([]byte, 1+16+4)
+	result[0] = byte(self.message)
+	copy(result[1:], self.source.IP)
+	result[len(result)-4] = byte(self.source.Port >> 24)
+	result[len(result)-3] = byte(self.source.Port >> 16)
+	result[len(result)-2] = byte(self.source.Port >> 8)
+	result[len(result)-1] = byte(self.source.Port)
+	return result
+}
+func decodeUdpMessage(message []byte) (result udpMessage) {
+	result.message = messageType(message[0])
+	result.source = &net.TCPAddr{make([]byte, 16), 0}
+	copy(result.source.IP, message[1:16+1])
+	result.source.Port += int(message[16+1]) << 24
+	result.source.Port += int(message[16+2]) << 16
+	result.source.Port += int(message[16+3]) << 8
+	result.source.Port += int(message[16+4])
+	return result
+}
+
+type Shard struct {
+	clusterAddressString string
+	clusterAddress       *net.UDPAddr
+	clusterConnection    *net.UDPConn
+	addressString        string
+	address              *net.TCPAddr
+}
+
+func NewShard(address string) *Shard {
+	return &Shard{
+		clusterAddressString: address,
+	}
+}
+func (self *Shard) String() string {
+	return fmt.Sprintf("%v@%v", self.addressString, self.clusterAddressString)
+}
+func (self *Shard) Verify() (err error) {
+	if self.clusterAddress == nil && self.address == nil {
+		if self.clusterAddress, err = net.ResolveUDPAddr("udp", self.clusterAddressString); err != nil {
+			return
+		}
+		if self.addressString == "" {
+			var tmpConn *net.UDPConn
+			if tmpConn, err = net.DialUDP("udp", nil, self.clusterAddress); err != nil {
+				return
+			}
+			self.addressString = tmpConn.LocalAddr().String()
+			tmpConn.Close()
+		}
+		self.address, err = net.ResolveTCPAddr("tcp", self.addressString)
+	}
 	return
 }
 func (self *Shard) listenMulticast() {
@@ -21,12 +87,12 @@ func (self *Shard) listenMulticast() {
 		panic(fmt.Errorf("While trying to open %v: %v", self.clusterAddress, err))
 	} else {
 		self.clusterConnection = connection
-		log.Printf("Started listening to %v", self.clusterConnection)
+		log.Printf("Started listening to %v", self.clusterAddress)
 		message := make([]byte, 512)
 		var err error
 		for err == nil {
 			if _, err = self.clusterConnection.Read(message); err == nil {
-				log.Printf("Got %v", message)
+				log.Printf("Got %v", decodeUdpMessage(message))
 			}
 		}
 		log.Printf("Stopped listening to %v: %v", self.clusterConnection, err)
@@ -46,7 +112,14 @@ func (self *Shard) multicast(message []byte) {
 		}
 	}
 }
-func (self *Shard) Start() {
+func (self *Shard) createUdpMessage(message messageType) udpMessage {
+	return udpMessage{message, self.address}
+}
+func (self *Shard) Start() (err error) {
+	if err = self.Verify(); err != nil {
+		return
+	}
 	go self.listenMulticast()
-	self.multicast([]byte("hehu!"))
+	self.multicast(self.createUdpMessage(join).encode())
+	return
 }
