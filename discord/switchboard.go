dca9af98ea6410762a82caaa00d8109a97592da8
Martin Bruse <martin@oort.se>
2012-10-17 15:17:41 +0200

    added an async method to switchboard

--- discord/switchboard.go
+++ discord/switchboard.go
@@ -15,7 +15,7 @@ type switchboard struct {
 func newSwitchboard() *switchboard {
 	return &switchboard{new(sync.RWMutex), make(map[string]*rpc.Client)}
 }
-func (self *switchboard) call(addr, service string, args, reply interface{}) (err error) {
+func (self *switchboard) client(addr) (client *rpc.Client, err error) {
 	self.lock.RLock()
 	client, ok := self.clients[addr]
 	self.lock.RUnlock()
@@ -26,6 +26,27 @@ func (self *switchboard) call(addr, service string, args, reply interface{}) (er
 		self.lock.Lock()
 		self.clients[addr] = client
 		self.lock.Unlock()
+	}
+	return
+}
+func (self *switchboard) acall(addr, service string, args, reply interface{}) (call *rpc.Call) {
+	if client, err := self.client(addr); err != nil {
+		call = &rpc.Call{
+			ServiceMethod: service,
+			Args:          args,
+			Reply:         reply,
+			Error:         err,
+			Done:          make(chan *rpc.Call, 1),
+		}
+		call.Done <- call
+	} else {
+		call = client.Go(service, args, reply, nil)
+	}
+	return
+}
+func (self *switchboard) call(addr, service string, args, reply interface{}) (err error) {
+	if client, err := self.client(addr); err != nil {
+		return
 	}
 	err = client.Call(service, args, reply)
 	return
