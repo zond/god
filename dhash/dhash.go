f666c32a5dae22a6ce4bf8f8178988c31cd83119
Martin Bruse <martin@oort.se>
2012-10-17 15:56:35 +0200

    added some persistence functionality to dhash

--- dhash/dhash.go
+++ dhash/dhash.go
@@ -4,7 +4,12 @@ import (
 	"../discord"
 	"../radix"
 	"../timenet"
+	"sync"
 	"time"
+)
+
+const (
+	redundancy = 3
 )
 
 type remotePeer discord.Remote
@@ -38,17 +43,20 @@ type Item struct {
 	Value     radix.Hasher
 	Exists    bool
 	Timestamp int64
+	TTL       int
 }
 
 type dhashServer DHash
 
 func (self *dhashServer) SlavePut(data Item, res *Item) error {
-	*res = data
-	res.Value, res.Exists = (*DHash)(self).tree.Put(data.Key, data.Value, data.Timestamp)
-	return nil
+	return (*DHash)(self).put(data, res)
+}
+func (self *dhashServer) Put(data Item, res *Item) error {
+	return (*DHash)(self).Put(data, res)
 }
 
 type DHash struct {
+	lock  *sync.RWMutex
 	node  *discord.Node
 	timer *timenet.Timer
 	tree  *radix.Tree
@@ -56,26 +64,61 @@ type DHash struct {
 
 func NewDHash(addr string) (result *DHash) {
 	result = &DHash{
+		lock: &sync.RWMutex{},
 		node: discord.NewNode(addr),
 		tree: radix.NewTree(),
 	}
 	result.timer = timenet.NewTimer((*dhashPeerProducer)(result))
 	result.node.Export("Timer", (*timerServer)(result.timer))
+	result.node.Export("DHash", (*dhashServer)(result))
 	return
 }
 func (self *DHash) MustStart() *DHash {
+	self.lock.RLock()
+	defer self.lock.RUnlock()
 	self.node.MustStart()
 	self.timer.Start()
 	return self
 }
 func (self *DHash) MustJoin(addr string) {
+	self.lock.RLock()
+	defer self.lock.RUnlock()
 	self.timer.Conform(remotePeer{Addr: addr})
 	self.node.MustJoin(addr)
 }
 func (self *DHash) Time() time.Time {
+	self.lock.RLock()
+	defer self.lock.RUnlock()
 	return time.Unix(0, self.timer.ContinuousTime())
 }
-func (self *DHash) Put(key []byte, value radix.Hasher) (old radix.Hasher, existed bool) {
-	timestamp := self.timer.ContinuousTime()
-	return self.tree.Put(key, value, timestamp)
+func (self *DHash) Put(data Item, res *Item) error {
+	self.lock.RLock()
+	if nodeCount := self.node.CountNodes(); nodeCount < redundancy {
+		data.TTL = nodeCount
+	} else {
+		data.TTL = redundancy
+	}
+	data.Timestamp = self.timer.ContinuousTime()
+	self.lock.RUnlock()
+	return self.put(data, res)
+}
+func (self *DHash) forwardPut(data Item) {
+	self.lock.RLock()
+	successor := self.node.GetSuccessor(self.node.GetPosition())
+	self.lock.RUnlock()
+	err := successor.Call("DHash.SlavePut", data, &data)
+	for err != nil {
+		self.lock.RLock()
+		self.node.RemoveNode(successor)
+		successor = self.node.GetSuccessor(self.node.GetPosition())
+		self.lock.RUnlock()
+		err = successor.Call("DHash.SlavePut", data, &data)
+	}
+}
+func (self *DHash) put(data Item, res *Item) error {
+	if data.TTL > 0 {
+		go self.forwardPut(data)
+	}
+	res.Value, res.Exists = self.tree.Put(data.Key, data.Value, data.Timestamp)
+	return nil
 }
