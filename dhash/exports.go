dbff68dc969b9018e4eb69c5b8a9baf6e8c397e1
Martin Bruse <martin@oort.se>
2012-10-18 16:29:18 +0200

    broke up the dhash.go a bit

---
+++ dhash/exports.go
@@ -0,0 +1,42 @@
+package dhash
+
+import (
+	"../common"
+	"../timenet"
+	"time"
+)
+
+type remotePeer common.Remote
+
+func (self remotePeer) ActualTime() (result int64) {
+	if err := (common.Remote)(self).Call("Timer.ActualTime", 0, &result); err != nil {
+		result = time.Now().UnixNano()
+	}
+	return
+}
+
+type dhashPeerProducer DHash
+
+func (self *dhashPeerProducer) Peers() (result map[string]timenet.Peer) {
+	result = make(map[string]timenet.Peer)
+	for _, node := range (*DHash)(self).node.GetNodes() {
+		result[node.Addr] = (remotePeer)(node)
+	}
+	return
+}
+
+type timerServer timenet.Timer
+
+func (self *timerServer) ActualTime(x int, result *int64) error {
+	*result = (*timenet.Timer)(self).ActualTime()
+	return nil
+}
+
+type dhashServer DHash
+
+func (self *dhashServer) SlavePut(data common.Item, res *common.Item) error {
+	return (*DHash)(self).put(data, res)
+}
+func (self *dhashServer) Put(data common.Item, res *common.Item) error {
+	return (*DHash)(self).Put(data, res)
+}
