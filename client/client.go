2372efd3719834ed88e71417dabafe1b2d59d468
Martin Bruse <martin@oort.se>
2012-10-18 15:26:51 +0200

    made client able to put data

--- client/client.go
+++ client/client.go
@@ -5,12 +5,12 @@ import (
 )
 
 type Conn struct {
-	nodes *common.Ring
+	ring *common.Ring
 }
 
 func NewConn(addr string) (result *Conn, err error) {
 	result = &Conn{&common.Ring{}}
-	err = common.Switch.Call(addr, "Node.Ring", 0, result.nodes)
+	err = common.Switch.Call(addr, "Node.Ring", 0, result.ring)
 	return
 }
 func MustConn(addr string) (result *Conn) {
@@ -20,6 +20,22 @@ func MustConn(addr string) (result *Conn) {
 	}
 	return
 }
+func (self *Conn) Put(key, value []byte) (old []byte, exists bool, err error) {
+	item := common.Item{
+		Key:   key,
+		Value: value,
+	}
+	predecessor, match, _ := self.ring.Remotes(key)
+	if match != nil {
+		predecessor = match
+	}
+	if err = predecessor.Call("DHash.Put", item, &item); err != nil {
+		return
+	}
+	old = item.Value
+	exists = item.Exists
+	return
+}
 func (self *Conn) Describe() string {
-	return self.nodes.Describe()
+	return self.ring.Describe()
 }
