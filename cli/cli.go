101b3443ca3e2aad12b6ed5e4f399197f048de39
Martin Bruse <martin@oort.se>
2012-10-18 13:47:37 +0200

    cli can now connect via cmd line flags

--- cli/cli.go
+++ cli/cli.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"../client"
+	"flag"
 	"fmt"
 )
 
+var ip = flag.String("ip", "127.0.0.1", "IP address to connect to")
+var port = flag.Int("port", 9191, "Port to connect to")
+
 func main() {
-	conn := client.MustConn("127.0.0.1:9191")
+	conn := client.MustConn(fmt.Sprintf("%v:%v", *ip, *port))
 	fmt.Println(conn.Describe())
 }
