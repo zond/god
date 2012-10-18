f928271788c518f38acab54d3306f19bdd8f5d0d
Martin Bruse <martin@oort.se>
2012-10-18 15:26:33 +0200

    made cli able to put data

--- cli/cli.go
+++ cli/cli.go
@@ -4,12 +4,40 @@ import (
 	"../client"
 	"flag"
 	"fmt"
+	"regexp"
+	"strings"
 )
+
+type action func(conn *client.Conn, args []string)
 
 var ip = flag.String("ip", "127.0.0.1", "IP address to connect to")
 var port = flag.Int("port", 9191, "Port to connect to")
 
-func main() {
-	conn := client.MustConn(fmt.Sprintf("%v:%v", *ip, *port))
+var actions = map[*regexp.Regexp]action{
+	regexp.MustCompile("^put (\\S+) (\\S+)$"): put,
+	regexp.MustCompile("^$"):                  show,
+}
+
+func show(conn *client.Conn, args []string) {
 	fmt.Println(conn.Describe())
+}
+
+func put(conn *client.Conn, args []string) {
+	old, existed, err := conn.Put([]byte(args[1]), []byte(args[2]))
+	if err != nil {
+		fmt.Println(err)
+	} else if existed {
+		fmt.Println(old)
+	}
+}
+
+func main() {
+	flag.Parse()
+	conn := client.MustConn(fmt.Sprintf("%v:%v", *ip, *port))
+	args := strings.Join(flag.Args(), " ")
+	for reg, fun := range actions {
+		if matches := reg.FindStringSubmatch(args); matches != nil {
+			fun(conn, matches)
+		}
+	}
 }
