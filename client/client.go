c5c0c392b71c7007f3df9ec998d5aee11b0216bd
Martin Bruse <martin@oort.se>
2012-10-19 13:22:48 +0200

    removed the error return values. added a parallell get

--- client/client.go
+++ client/client.go
@@ -3,6 +3,7 @@ package client
 import (
 	"../common"
 	"fmt"
+	"net/rpc"
 )
 
 type Conn struct {
@@ -21,17 +22,56 @@ func MustConn(addr string) (result *Conn) {
 	}
 	return
 }
-func (self *Conn) Put(key, value []byte) (err error) {
-	item := common.Item{
+func (self *Conn) Reconnect() {
+	_, _, successor := self.ring.Remotes(nil)
+	var err error
+	for {
+		if err = successor.Call("Node.Ring", 0, self.ring); err == nil {
+			return
+		}
+		self.ring.Remove(*successor)
+		_, _, successor = self.ring.Remotes(nil)
+	}
+}
+func (self *Conn) Put(key, value []byte) {
+	data := common.Item{
 		Key:   key,
 		Value: value,
 	}
-	predecessor, match, _ := self.ring.Remotes(key)
-	if match != nil {
-		predecessor = match
-	}
+	_, _, successor := self.ring.Remotes(key)
 	var x int
-	err = predecessor.Call("DHash.Put", item, &x)
+	if err := successor.Call("DHash.Put", data, &x); err != nil {
+		self.Reconnect()
+		self.Put(key, value)
+	}
+}
+func (self *Conn) Get(key []byte) (value []byte, existed bool) {
+	data := common.Item{
+		Key: key,
+	}
+	futures := make([]*rpc.Call, common.Redundancy)
+	results := make([]*common.Item, common.Redundancy)
+	nextKey := key
+	var nextSuccessor *common.Remote
+	for i := 0; i < common.Redundancy; i++ {
+		_, _, nextSuccessor = self.ring.Remotes(nextKey)
+		result := &common.Item{}
+		results[i] = result
+		futures[i] = nextSuccessor.Go("DHash.Get", data, result)
+		_, _, nextSuccessor = self.ring.Remotes(nextSuccessor.Pos)
+	}
+	var result *common.Item
+	for index, future := range futures {
+		<-future.Done
+		if future.Error != nil {
+			self.Reconnect()
+			return self.Get(key)
+		}
+		if result == nil || result.Timestamp < results[index].Timestamp {
+			result = results[index]
+		}
+	}
+	value, existed = result.Value, result.Exists
 	return
 }
 func (self *Conn) DescribeTree(key []byte) (result string, err error) {
