9e9a12f9b402438a76ccd8f3c24c0798c64e927a
Martin Bruse <martin@oort.se>
2012-10-26 10:31:39 +0200

    added a GetPredecessor

--- discord/node_server.go
+++ discord/node_server.go
@@ -18,6 +18,10 @@ func (self *nodeServer) Ping(x int, y *int) error {
 	(*Node)(self).Ping()
 	return nil
 }
+func (self *nodeServer) GetPredecessor(x int, predecessor *common.Remote) error {
+	*predecessor = (*Node)(self).GetPredecessor()
+	return nil
+}
 func (self *nodeServer) GetSuccessor(key []byte, successor *common.Remote) error {
 	*successor = (*Node)(self).GetSuccessor(key)
 	return nil
