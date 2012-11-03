14be0fcce05dea75ddfd067694a0e62b17b1c138
Martin Bruse <zondolfin@gmail.com>
2012-11-02 22:03:05 -0700

    exported DHash#SubFind

--- dhash/dhash_server.go
+++ dhash/dhash_server.go
@@ -21,6 +21,9 @@ func (self *dhashServer) Put(data common.Item, x *int) error {
 func (self *dhashServer) RingHash(x int, result *[]byte) error {
 	return (*DHash)(self).RingHash(x, result)
 }
+func (self *dhashServer) SubFind(data common.Item, result *common.Item) error {
+	return (*DHash)(self).SubFind(data, result)
+}
 func (self *dhashServer) Find(data common.Item, result *common.Item) error {
 	return (*DHash)(self).Find(data, result)
 }
