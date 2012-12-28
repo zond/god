6c3bd53a849a6bfa75ffd673858594e517826293
Martin Bruse <zondolfin@gmail.com>
2012-12-28 12:26:01 +0100

    more godoc

--- radix/radix.go
+++ radix/radix.go
@@ -8,6 +8,9 @@ import (
 
 var encodeChars = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"}
 
+// To limit the number of children of each Node in the Tree, we explode any byte slice keys into nibble slices. But to avoid hairy bit operations
+// we let each nibble live inside a complete byte, and only use least significant half of it. To let the compiler help us keep track of this
+// all byte slices that are supposed to be exploded are Nibble slices.
 type Nibble byte
 
 type Timer interface {
