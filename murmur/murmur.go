45e9275779dc1debe5f855d87801fe0ad7b4ffe9
Martin Bruse <zondolfin@gmail.com>
2012-12-28 12:18:13 +0100

    more godoc

--- murmur/murmur.go
+++ murmur/murmur.go
@@ -23,38 +23,52 @@ const (
 	seed      = 42
 )
 
+// HashString will return the hash for the provided string.
 func HashString(s string) []byte {
 	return NewBytes([]byte(s)).Get()
 }
 
+// HashInt will return the hash for the provided int.
 func HashInt(i int) []byte {
 	return NewString(fmt.Sprint(i)).Get()
 }
 
+// HashInt64 will return the hash for the provided int64.
 func HashInt64(i int64) []byte {
 	return NewString(fmt.Sprint(i)).Get()
 }
 
+// HashBytes will return the hash for the provided byte slice.
 func HashBytes(b []byte) []byte {
 	return NewBytes(b).Get()
 }
 
+// Hash is a thin thin wrapper around the reference implementation of http://code.google.com/p/smhasher/wiki/MurmurHash3 modified slightly to compile with a C compiler.
 type Hash bytes.Buffer
 
 func New() *Hash {
 	return new(Hash)
 }
+
+// NewString will return a Hash that has already consumed the provided string.
 func NewString(s string) *Hash {
 	return (*Hash)(bytes.NewBufferString(s))
 }
+
+// NewBytes will return a Hash that has already consumed the provided byte slice.
 func NewBytes(b []byte) *Hash {
 	return (*Hash)(bytes.NewBuffer(b))
 }
+
+// Get will return the hash for all consumed data.
 func (self *Hash) Get() (result []byte) {
 	result = make([]byte, Size)
 	self.Extrude(&result)
 	return
 }
+
+// Extrude will fill the provided slice with the hash for all consumed data.
+// Extrude is slightly less allocating than Get, if that kind of thing is of interest.
 func (self *Hash) Extrude(result *[]byte) {
 	buf := (*bytes.Buffer)(self).Bytes()
 	C.MurmurHash3_x64_128(
@@ -64,18 +78,26 @@ func (self *Hash) Extrude(result *[]byte) {
 		*(*unsafe.Pointer)(unsafe.Pointer(result)))
 	self.Reset()
 }
+
+// Sum will consume the given bytes and return the hash for all consumed data.
 func (self *Hash) Sum(p []byte) []byte {
 	(*bytes.Buffer)(self).Write(p)
 	return self.Get()
 }
+
+// MustWrite will consume the provided bytes.
 func (self *Hash) MustWrite(p []byte) {
 	if i, err := (*bytes.Buffer)(self).Write(p); i != len(p) || err != nil {
 		panic(fmt.Errorf("When Writing %v to %v, got %v, %v", p, self, i, err))
 	}
 }
+
+// Write will consume the provided bytes, or return an error.
 func (self *Hash) Write(p []byte) (n int, err error) {
 	return (*bytes.Buffer)(self).Write(p)
 }
+
+// Reset will forget all consumed data.
 func (self *Hash) Reset() {
 	(*bytes.Buffer)(self).Truncate(0)
 }
