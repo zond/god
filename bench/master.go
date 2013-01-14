a09a89229e8a321e3fa3524efea9c2d383b112c6
Martin Bruse <zondolfin@gmail.com>
2013-01-14 10:56:25 +0100

    the benchmarks now output only NODES TAB SIZE TAB RPS

--- bench/master.go
+++ bench/master.go
@@ -1,61 +1,66 @@
 package bench
 
 import (
-	"flag"
-	"fmt"
-	"net/rpc"
+  "flag"
+  "fmt"
+  "net/rpc"
 )
 
 func RunMaster() {
-	var ip = flag.String("ip", "127.0.0.1", "IP address to find a node at")
-	var port = flag.Int("port", 9191, "Port to find a node at")
-	var maxKey = flag.Int64("maxKey", 10000, "Biggest key as int64 converted to []byte using common.EncodeInt64")
-	var prepare = flag.Bool("prepare", false, "Whether to make sure that all keys within the maxKey range are set before starting")
-	flag.Parse()
-	clients := make([]*rpc.Client, len(flag.Args()))
-	rps := make([]float64, len(flag.Args()))
-	var err error
-	for index, addr := range flag.Args() {
-		if clients[index], err = rpc.Dial("tcp", addr); err != nil {
-			panic(err)
-		}
-	}
-	command := SpinCommand{
-		Addr:   fmt.Sprintf("%v:%v", *ip, *port),
-		MaxKey: *maxKey,
-	}
-	if *prepare {
-		calls := make([]*rpc.Call, len(clients))
-		for index, client := range clients {
-			calls[index] = client.Go("Slave.Prepare", PrepareCommand{
-				Addr:  command.Addr,
-				Range: [2]int64{int64(index) * (*maxKey / int64(len(clients))), (int64(index) + 1) * (*maxKey / int64(len(clients)))},
-			}, &Nothing{}, nil)
-		}
-		for _, call := range calls {
-			<-call.Done
-			if call.Error != nil {
-				panic(call.Error)
-			}
-		}
-	}
-	for index, client := range clients {
-		if err = client.Call("Slave.Spin", command, &Nothing{}); err == nil {
-			fmt.Println(flag.Args()[index], "is alive")
-		} else {
-			panic(err)
-		}
-	}
-	for index, client := range clients {
-		if err = client.Call("Slave.Wait", Nothing{}, &(rps[index])); err == nil {
-			fmt.Println(flag.Args()[index], "peaked")
-		} else {
-			panic(err)
-		}
-	}
-	sum := float64(0)
-	for _, r := range rps {
-		sum += r
-	}
-	fmt.Println("Peaked at", sum, "rps")
+  var ip = flag.String("ip", "127.0.0.1", "IP address to find a node at")
+  var port = flag.Int("port", 9191, "Port to find a node at")
+  var maxKey = flag.Int64("maxKey", 10000, "Biggest key as int64 converted to []byte using common.EncodeInt64")
+  var prepare = flag.Bool("prepare", false, "Whether to make sure that all keys within the maxKey range are set before starting")
+  flag.Parse()
+  clients := make([]*rpc.Client, len(flag.Args()))
+  rps := make([]float64, len(flag.Args()))
+  var err error
+  for index, addr := range flag.Args() {
+    if clients[index], err = rpc.Dial("tcp", addr); err != nil {
+      panic(err)
+    }
+  }
+  command := SpinCommand{
+    Addr:   fmt.Sprintf("%v:%v", *ip, *port),
+    MaxKey: *maxKey,
+  }
+  if *prepare {
+    calls := make([]*rpc.Call, len(clients))
+    for index, client := range clients {
+      calls[index] = client.Go("Slave.Prepare", PrepareCommand{
+        Addr:  command.Addr,
+        Range: [2]int64{int64(index) * (*maxKey / int64(len(clients))), (int64(index) + 1) * (*maxKey / int64(len(clients)))},
+      }, &Nothing{}, nil)
+    }
+    for _, call := range calls {
+      <-call.Done
+      if call.Error != nil {
+        panic(call.Error)
+      }
+    }
+  }
+  var oldSpinRes *SpinResult
+  var spinRes SpinResult
+  for _, client := range clients {
+    if err = client.Call("Slave.Spin", command, &spinRes); err != nil {
+      panic(err)
+    }
+    if oldSpinRes == nil {
+      oldSpinRes = &spinRes
+    } else {
+      if spinRes.Keys != oldSpinRes.Keys || spinRes.Nodes != oldSpinRes.Nodes {
+        panic(fmt.Errorf("Last slave had %v nodes and %v keys, now I get %v nodes and %v keys?", oldSpinRes.Nodes, oldSpinRes.Keys, spinRes.Nodes, spinRes.Keys))
+      }
+    }
+  }
+  for index, client := range clients {
+    if err = client.Call("Slave.Wait", Nothing{}, &(rps[index])); err != nil {
+      panic(err)
+    }
+  }
+  sum := float64(0)
+  for _, r := range rps {
+    sum += r
+  }
+  fmt.Printf("%v\t%v\t%v\n", spinRes.Nodes, spinRes.Keys, sum)
 }
--- bench/slave.go
+++ bench/slave.go
@@ -1,140 +1,149 @@
 package bench
 
 import (
-	"flag"
-	"fmt"
-	"github.com/zond/god/client"
-	"github.com/zond/god/murmur"
-	"math/rand"
-	"net"
-	"net/rpc"
-	"sync"
-	"sync/atomic"
-	"time"
+  "flag"
+  "fmt"
+  "github.com/zond/god/client"
+  "github.com/zond/god/murmur"
+  "math/rand"
+  "net"
+  "net/rpc"
+  "sync"
+  "sync/atomic"
+  "time"
 )
 
 func init() {
-	rand.Seed(time.Now().UnixNano())
+  rand.Seed(time.Now().UnixNano())
 }
 
 const (
-	stopped = iota
-	started
+  stopped = iota
+  started
 )
 
+type SpinResult struct {
+  Nodes int
+  Keys  int
+}
+
 type PrepareCommand struct {
-	Addr  string
-	Range [2]int64
+  Addr  string
+  Range [2]int64
 }
 
 type SpinCommand struct {
-	Addr   string
-	MaxKey int64
+  Addr   string
+  MaxKey int64
 }
 
 type Nothing struct{}
 
 type Slave struct {
-	maxRps float64
-	maxKey int64
-	req    int64
-	start  time.Time
-	addr   string
-	state  int32
-	client *client.Conn
-	wg     *sync.WaitGroup
+  maxRps float64
+  maxKey int64
+  req    int64
+  start  time.Time
+  addr   string
+  state  int32
+  client *client.Conn
+  wg     *sync.WaitGroup
 }
 
 func (self *Slave) switchState(expected, wanted int32) bool {
-	return atomic.CompareAndSwapInt32(&self.state, expected, wanted)
+  return atomic.CompareAndSwapInt32(&self.state, expected, wanted)
 }
 func (self *Slave) hasState(s int32) bool {
-	return atomic.LoadInt32(&self.state) == s
+  return atomic.LoadInt32(&self.state) == s
 }
 
 func (self *Slave) spinner() {
-	var kv []byte
-	for self.hasState(started) {
-		kv = murmur.HashInt64(rand.Int63n(self.maxKey))
-		self.client.Put(kv, kv)
-		atomic.AddInt64(&self.req, 1)
-	}
+  var kv []byte
+  for self.hasState(started) {
+    kv = murmur.HashInt64(rand.Int63n(self.maxKey))
+    self.client.Put(kv, kv)
+    atomic.AddInt64(&self.req, 1)
+  }
 }
 
 func (self *Slave) run() {
-	var currRps float64
-	for self.hasState(started) {
-		currRps = float64(atomic.LoadInt64(&self.req)) / (float64(time.Now().UnixNano()-self.start.UnixNano()) / float64(time.Second))
-		if self.maxRps == 0 || currRps > self.maxRps {
-			fmt.Println("Spinning up one more loader, ", currRps, ">", self.maxRps)
-			self.maxRps = currRps
-			self.req = 0
-			self.start = time.Now()
-			go self.spinner()
-			time.Sleep(time.Second)
-		} else {
-			fmt.Println("Peaked at", self.maxRps)
-			self.wg.Done()
-			return
-		}
-	}
+  var currRps float64
+  for self.hasState(started) {
+    currRps = float64(atomic.LoadInt64(&self.req)) / (float64(time.Now().UnixNano()-self.start.UnixNano()) / float64(time.Second))
+    if self.maxRps == 0 || currRps > self.maxRps {
+      fmt.Println("Spinning up one more loader, ", currRps, ">", self.maxRps)
+      self.maxRps = currRps
+      self.req = 0
+      self.start = time.Now()
+      go self.spinner()
+      time.Sleep(time.Second)
+    } else {
+      fmt.Println("Peaked at", self.maxRps)
+      self.wg.Done()
+      return
+    }
+  }
 }
 
 func (self *Slave) Prepare(command PrepareCommand, x *Nothing) error {
-	if self.hasState(stopped) {
-		fmt.Println("Preparing on ", command.Addr)
-		self.client = client.MustConn(command.Addr)
-		var kv []byte
-		for i := command.Range[0]; i < command.Range[1]; i++ {
-			kv = murmur.HashInt64(i)
-			self.client.Put(kv, kv)
-			if i%1000 == 0 {
-				fmt.Print(".")
-			}
-		}
-		fmt.Println("done!")
-	}
-	return nil
+  if self.hasState(stopped) {
+    fmt.Println("Preparing on ", command.Addr)
+    self.client = client.MustConn(command.Addr)
+    var kv []byte
+    for i := command.Range[0]; i < command.Range[1]; i++ {
+      kv = murmur.HashInt64(i)
+      self.client.Put(kv, kv)
+      if i%1000 == 0 {
+        fmt.Print(".")
+      }
+    }
+    fmt.Println("done!")
+  }
+  return nil
 }
 
 func (self *Slave) Wait(x Nothing, rps *float64) error {
-	if self.hasState(started) {
-		self.wg.Wait()
-		self.switchState(started, stopped)
-		*rps = self.maxRps
-		return nil
-	}
-	return fmt.Errorf("%v is not started", self)
+  if self.hasState(started) {
+    self.wg.Wait()
+    self.switchState(started, stopped)
+    *rps = self.maxRps
+    return nil
+  }
+  return fmt.Errorf("%v is not started", self)
 }
 
-func (self *Slave) Spin(command SpinCommand, o *Nothing) error {
-	if self.switchState(stopped, started) {
-		fmt.Println("Spinning on ", command.Addr)
-		self.wg = new(sync.WaitGroup)
-		self.wg.Add(1)
-		self.maxRps = 0
-		self.client = client.MustConn(command.Addr)
-		self.maxKey = command.MaxKey
-		go self.run()
-	} else {
-		fmt.Println("Already started on", self.addr)
-	}
-	return nil
+func (self *Slave) Spin(command SpinCommand, result *SpinResult) error {
+  if self.switchState(stopped, started) {
+    fmt.Println("Spinning on ", command.Addr)
+    self.wg = new(sync.WaitGroup)
+    self.wg.Add(1)
+    self.maxRps = 0
+    self.client = client.MustConn(command.Addr)
+    self.maxKey = command.MaxKey
+    go self.run()
+  } else {
+    fmt.Println("Already started on", self.addr)
+  }
+  *result = SpinResult{
+    Nodes: len(self.client.Nodes()),
+    Keys:  self.client.Size(),
+  }
+  return nil
 }
 
 func RunSlave() {
-	var ip = flag.String("ip", "127.0.0.1", "IP address to listen to")
-	var port = flag.Int("port", 19191, "Port to connect to")
-	flag.Parse()
-	var err error
-	var addr *net.TCPAddr
-	if addr, err = net.ResolveTCPAddr("tcp", fmt.Sprintf("%v:%v", *ip, *port)); err != nil {
-		panic(err)
-	}
-	var listener *net.TCPListener
-	if listener, err = net.ListenTCP("tcp", addr); err != nil {
-		panic(err)
-	}
-	rpc.Register(&Slave{})
-	rpc.Accept(listener)
+  var ip = flag.String("ip", "127.0.0.1", "IP address to listen to")
+  var port = flag.Int("port", 19191, "Port to connect to")
+  flag.Parse()
+  var err error
+  var addr *net.TCPAddr
+  if addr, err = net.ResolveTCPAddr("tcp", fmt.Sprintf("%v:%v", *ip, *port)); err != nil {
+    panic(err)
+  }
+  var listener *net.TCPListener
+  if listener, err = net.ListenTCP("tcp", addr); err != nil {
+    panic(err)
+  }
+  rpc.Register(&Slave{})
+  rpc.Accept(listener)
 }
