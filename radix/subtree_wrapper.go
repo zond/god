48ae3c190fa046dd69c6bc25794632b4574d0f40
Martin Bruse <zondolfin@gmail.com>
2013-02-03 12:45:50 +0100

    reindent, and SubClearTimestamp

--- radix/subtree_wrapper.go
+++ radix/subtree_wrapper.go
@@ -1,49 +1,52 @@
 package radix
 
 type subTreeWrapper struct {
-	parentTree HashTree
-	key        []Nibble
+  parentTree HashTree
+  key        []Nibble
 }
 
 func (self *subTreeWrapper) Configuration() (map[string]string, int64) {
-	return self.parentTree.SubConfiguration(Stitch(self.key))
+  return self.parentTree.SubConfiguration(Stitch(self.key))
 }
 func (self *subTreeWrapper) Configure(conf map[string]string, ts int64) {
-	self.parentTree.SubConfigure(Stitch(self.key), conf, ts)
+  self.parentTree.SubConfigure(Stitch(self.key), conf, ts)
 }
 func (self *subTreeWrapper) Hash() (hash []byte) {
-	if p := self.parentTree.Finger(self.key); p != nil {
-		hash = p.TreeHash
-	}
-	return
+  if p := self.parentTree.Finger(self.key); p != nil {
+    hash = p.TreeHash
+  }
+  return
 }
 func (self *subTreeWrapper) Finger(subKey []Nibble) *Print {
-	return self.parentTree.SubFinger(self.key, subKey)
+  return self.parentTree.SubFinger(self.key, subKey)
 }
 func (self *subTreeWrapper) GetTimestamp(subKey []Nibble) (byteValue []byte, version int64, present bool) {
-	return self.parentTree.SubGetTimestamp(self.key, subKey)
+  return self.parentTree.SubGetTimestamp(self.key, subKey)
 }
 func (self *subTreeWrapper) PutTimestamp(subKey []Nibble, byteValue []byte, present bool, expected, version int64) bool {
-	return self.parentTree.SubPutTimestamp(self.key, subKey, byteValue, present, expected, version)
+  return self.parentTree.SubPutTimestamp(self.key, subKey, byteValue, present, expected, version)
 }
 func (self *subTreeWrapper) DelTimestamp(subKey []Nibble, expected int64) bool {
-	return self.parentTree.SubDelTimestamp(self.key, subKey, expected)
+  return self.parentTree.SubDelTimestamp(self.key, subKey, expected)
 }
 func (self *subTreeWrapper) SubConfiguration(key []byte) (map[string]string, int64) {
-	panic(subTreeError)
+  panic(subTreeError)
 }
 func (self *subTreeWrapper) SubConfigure(key []byte, conf map[string]string, ts int64) {
-	panic(subTreeError)
+  panic(subTreeError)
 }
 func (self *subTreeWrapper) SubFinger(key, subKey []Nibble) (result *Print) {
-	panic(subTreeError)
+  panic(subTreeError)
 }
 func (self *subTreeWrapper) SubGetTimestamp(key, subKey []Nibble) (byteValue []byte, version int64, present bool) {
-	panic(subTreeError)
+  panic(subTreeError)
 }
 func (self *subTreeWrapper) SubPutTimestamp(key, subKey []Nibble, byteValue []byte, present bool, subExpected, subTimestamp int64) bool {
-	panic(subTreeError)
+  panic(subTreeError)
 }
 func (self *subTreeWrapper) SubDelTimestamp(key, subKey []Nibble, subExpected int64) bool {
-	panic(subTreeError)
+  panic(subTreeError)
+}
+func (self *subTreeWrapper) SubClearTimestamp(key []Nibble, expected int64) bool {
+  panic(subTreeError)
 }
